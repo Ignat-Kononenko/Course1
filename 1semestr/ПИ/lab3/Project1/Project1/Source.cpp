2502459b4d4222a903382ce63bb69b558c339c9e
Ignat <151752700+Ignat-Kononenko@users.noreply.github.com>
2025-01-01 17:44:16 +0300

    Add files via upload

---
+++ 1semestr/ПИ/lab3/Project1/Project1/Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+//KononenkoIgnat2006
+//4b 6f 6e 6f 6e 65 6e 6b 6f 49 67 6e 61 74 32 30 30 36 00
+//4B 6F	6E 6F 6E 65 6E 6B 6F 49 67 6E 61 74 32 30 30 36
+//004B 006F 006E 006F 006E 0065 006E 006B 006F 0049 0067 006E 0061 0074 0032 0030 0030 0036
+
+//КононенкоИгнат2006
+//ca ee ed  ee ed e5 ed ea ee c8 e3 ed e0 f2 32 30 30 36 00
+//D09A D0BF D0BD D0BF D0BD D0B5 D0BD D0BA D0BF D098 D0B3 D0BD D0B0 D182 32 30 30 36
+//041A 043E 043D 043E 043D 04345 043D 043A 043E 0418 0433 043D 0430 0442 0032 0030 0030 0036
+
+//Кононенко2006Ignat
+//ca ee ed  ee ed e5 ed ea ee 32 30 30 36 49 67 6e 61 74 00
+//D09A D0BF D0BD D0BF D0BD D0B5 D0BD D0BA D0BF 32 30 30 36 49 67 6E 61 74
+//041A 043E 043D 043E 043D 04345 043D 043A 043E 0032 0030 0030 0036 0049 0067 006E 0061 0074
+
+using namespace std;
+int main()
+{
+	int number = 0x12345678;
+	char name[] = "KononenkoIgnat2006";
+	char rname[] = "КононенкоИгнат2006";
+	char rlname[] = "Кононенко2006Ignat";
+	cout << name;
+return 0;
+}
